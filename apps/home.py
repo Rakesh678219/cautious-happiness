9b7aa38529ab6605cb2bb94edbad616d7ba04e14
Rakesh678219 <67853735+Rakesh678219@users.noreply.github.com>
2021-06-27 19:39:07 +0530

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 def app():
     st.header('Eyes the Windows to Your Health ')
-    video_file = open('videos/intro.mp4', 'rb')
+    video_file = open('intro.mp4', 'rb')
     video_bytes = video_file.read()
     st.video(video_bytes, start_time=1)
 
@@ -62,13 +62,13 @@ def app():
     why the eye's tissues and blood vessels make such a good barometer for wellness** """)
 
     st.header('Animation: Dilated Eye Exam')
-    video_file = open('videos/Animation_ Dilated Eye Exam.mp4', 'rb')
+    video_file = open('Animation_ Dilated Eye Exam.mp4', 'rb')
     video_bytes = video_file.read()
     st.video(video_bytes, start_time=1)
     st.write('** This video describes what the doctor sees when examining the retina (the light-sensitive tissue at the back of the eye), macula (the part of the retina needed for sharp, central vision) and optic nerve (which connects the retina to the brain).**')
 
     st.header('How Your Eyes Make Sense of the World ?')
-    video_file = open('videos/eye_decoder.mp4', 'rb')
+    video_file = open('eye_decoder.mp4', 'rb')
     video_bytes = video_file.read()
     st.video(video_bytes, start_time=1)
     st.write('** How does the eye work exactly?  learn some of the extraordinary science behind how your eyes and brain work together to perceive the world around you **')
