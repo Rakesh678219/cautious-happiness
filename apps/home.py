e58d7b8fecc339dfc3a2932f534e926ac003ee89
Rakesh678219 <67853735+Rakesh678219@users.noreply.github.com>
2021-06-27 19:29:31 +0530

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -6,25 +6,7 @@ def app():
     video_file = open('intro.mp4', 'rb')
     video_bytes = video_file.read()
     st.video(video_bytes, start_time=1)
-
-    st.write(""" **Your eyes are tiny spheres of wonder. 
-    A doctor can find warning signs of high blood pressure, diabetes, and a whole range of other
-    systemic health issues, just by examining your eyes. Ophthalmologist Neal Adams explains
-    why the eye's tissues and blood vessels make such a good barometer for wellness** """)
-
-    st.header('Animation: Dilated Eye Exam')
-    video_file = open('Animation_ Dilated Eye Exam.mp4', 'rb')
-    video_bytes = video_file.read()
-    st.video(video_bytes, start_time=1)
-    st.write('** This video describes what the doctor sees when examining the retina (the light-sensitive tissue at the back of the eye), macula (the part of the retina needed for sharp, central vision) and optic nerve (which connects the retina to the brain).**')
-
-    st.header('How Your Eyes Make Sense of the World ?')
-    video_file = open('eye_decoder.mp4', 'rb')
-    video_bytes = video_file.read()
-    st.video(video_bytes, start_time=1)
-    st.write('** How does the eye work exactly?  learn some of the extraordinary science behind how your eyes and brain work together to perceive the world around you **')
-
-    def draw_all(
+        def draw_all(
         key,
         plot=False,
     ):
@@ -72,3 +54,22 @@ def app():
 
     with st.sidebar:
         draw_all("sidebar")
+
+
+    st.write(""" **Your eyes are tiny spheres of wonder. 
+    A doctor can find warning signs of high blood pressure, diabetes, and a whole range of other
+    systemic health issues, just by examining your eyes. Ophthalmologist Neal Adams explains
+    why the eye's tissues and blood vessels make such a good barometer for wellness** """)
+
+    st.header('Animation: Dilated Eye Exam')
+    video_file = open('Animation_ Dilated Eye Exam.mp4', 'rb')
+    video_bytes = video_file.read()
+    st.video(video_bytes, start_time=1)
+    st.write('** This video describes what the doctor sees when examining the retina (the light-sensitive tissue at the back of the eye), macula (the part of the retina needed for sharp, central vision) and optic nerve (which connects the retina to the brain).**')
+
+    st.header('How Your Eyes Make Sense of the World ?')
+    video_file = open('eye_decoder.mp4', 'rb')
+    video_bytes = video_file.read()
+    st.video(video_bytes, start_time=1)
+    st.write('** How does the eye work exactly?  learn some of the extraordinary science behind how your eyes and brain work together to perceive the world around you **')
+
